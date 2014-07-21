544013c7f2aeb3d9fd38cf2bd173750c55398606
Umair Khan <omerjerk@gmail.com>
2014-07-21 14:14:07 +0530

    Initial commit with basic activities
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

---
+++ app/src/main/java/in/tosc/remotedroid/app/MainActivity.java
@@ -0,0 +1,46 @@
+package in.tosc.remotedroid.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void startClient (View v) {
+        startActivity(new Intent(MainActivity.this, ClientActivity.class));
+    }
+
+    public void startServer (View v) {
+        startActivity(new Intent(MainActivity.this, ServerActivity.class));
+    }
+}
