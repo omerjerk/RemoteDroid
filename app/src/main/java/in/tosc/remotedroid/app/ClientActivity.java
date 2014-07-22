f82bc5cc88b2a22e52fdc89e2d8ec36c64e18a3b
Umair Khan <omerjerk@gmail.com>
2014-07-23 01:29:59 +0530

    Client : use websockets instead of socketio
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/src/main/java/in/tosc/remotedroid/app/ClientActivity.java
+++ app/src/main/java/in/tosc/remotedroid/app/ClientActivity.java
@@ -3,40 +3,43 @@ package in.tosc.remotedroid.app;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import com.koushikdutta.async.AsyncSocket;
 import com.koushikdutta.async.ByteBufferList;
 import com.koushikdutta.async.DataEmitter;
-import com.koushikdutta.async.http.socketio.*;
+import com.koushikdutta.async.http.WebSocket;
 import com.koushikdutta.async.callback.DataCallback;
 import com.koushikdutta.async.http.AsyncHttpClient;
-import com.koushikdutta.async.http.socketio.transport.SocketIOTransport;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 
 public class ClientActivity extends Activity {
+
+    private static final String TAG = "omerjerk";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
-        SocketIOClient.connect(AsyncHttpClient.getDefaultInstance(), "http://192.168.1.2:3000", connectCallback);
+        AsyncHttpClient.getDefaultInstance().websocket("ws://192.168.43.1:6000", "ws", websocketCallback);
+
     }
 
-    private ConnectCallback connectCallback = new ConnectCallback() {
+    private AsyncHttpClient.WebSocketConnectCallback websocketCallback = new AsyncHttpClient.WebSocketConnectCallback() {
         @Override
-        public void onConnectCompleted(Exception e, SocketIOClient socketIOClient) {
-            if (e != null) {
-                e.printStackTrace();
+        public void onCompleted(Exception ex, WebSocket webSocket) {
+            if (ex != null) {
+                ex.printStackTrace();
                 return;
             }
-
-            socketIOClient.setStringCallback(new StringCallback() {
+            webSocket.send("a string");
+            webSocket.send(new byte[10]);
+            webSocket.setStringCallback(new WebSocket.StringCallback() {
+                public void onStringAvailable(String s) {
+                    System.out.println("I got a string: " + s);
+                }
+            });
+            webSocket.setDataCallback(new DataCallback() {
                 @Override
-                public void onString(String s, Acknowledge acknowledge) {
-
+                public void onDataAvailable(DataEmitter dataEmitter, ByteBufferList byteBufferList) {
+                    Log.d(TAG, "I got some bytes = " + byteBufferList.toString());
                 }
             });
         }
